36f218b47acdcdb993f354c984fbcc6200a5f4c6
Akash Pushkar <akashpushkar02@gmail.com>
2021-12-07 04:56:29 -0500

    conversion of spherical cooridiantes to xyz in read scan

--- pye57/utils.py
+++ pye57/utils.py
@@ -1,6 +1,9 @@
+from typing import Type
+
 from pye57 import libe57
 from pye57.libe57 import NodeType
 
+import numpy as np
 
 def get_fields(node):
     return [node.get(id_).elementName() for id_ in range(node.childCount())]
@@ -19,3 +22,17 @@ def get_node(node, name):
     }
     n = node.get(name)
     return cast[n.type()](n)
+
+def convert_spherical_to_cartesian(rae: Type[np.ndarray])-> Type[np.ndarray]:
+    """
+    Converts spherical(rae) to cartesian(xyz), where rae = range, azimuth(theta), 
+    elevation(phi). 
+    
+    Reference for formula: http://www.libe57.org/bestCoordinates.html (Note: the 
+    formula is different from the one online, so please one at the above reference)
+    """
+    return np.concatenate((
+            rae[:, :1] * np.cos(rae[:, 2:3]) * np.cos(rae[:, 1:2]),
+            rae[:, :1] * np.cos(rae[:, 2:3]) * np.sin(rae[:, 1:2]),
+            rae[:, :1] * np.sin(rae[:, 2:3])
+        ), axis=1)
