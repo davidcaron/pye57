1bdf08285dcf6136798970e9a2f4bf4487d6e1cc
Aman Gupta <26127617+amangupta17@users.noreply.github.com>
2024-07-24 15:04:16 -0700

    feat: add macos support

--- scripts/install_xerces_c.sh
+++ scripts/install_xerces_c.sh
@@ -7,25 +7,32 @@ XERCES_MINOR=2
 XERCES_PATCH=3
 
 XERCES_VERSION=${XERCES_MAJOR}.${XERCES_MINOR}.${XERCES_PATCH}
-XSD_BUILD_AREA=/tmp/build_xerces_c
-
-echo "Create Xerces-C build area..."
-mkdir -p ${XSD_BUILD_AREA}
-cd ${XSD_BUILD_AREA}
 
 echo "Download Xerces-C..."
 curl -L https://github.com/apache/xerces-c/archive/v${XERCES_VERSION}.tar.gz --output xerces-c-${XERCES_VERSION}.tar.gz
 echo "Extract Xerces-C..."
 tar xzf xerces-c-${XERCES_VERSION}.tar.gz
-cd  xerces-c-${XERCES_VERSION}
-echo "Configure Xerces-C..."
-./reconf
-./configure
+cd xerces-c-${XERCES_VERSION}
+
+# Check the operating system and install dependencies accordingly
+if [[ "$(uname)" == "Darwin" ]]; then
+    echo "Installing dependencies on macOS..."
+    brew install autoconf automake libtool
+else
+    echo "Installing dependencies on Linux..."
+    sudo apt-get install -y autoconf automake libtool
+fi
+
+echo "Generate configure script using autoreconf..."
+autoreconf -i
+
+echo "Configure Xerces-C for macOS..."
+./configure --prefix=/usr/local --enable-static
+
 echo "Build Xerces-C..."
 make
-echo "Install Xerces-C..."
-make install
 
-echo "Clean up Xerces-C..."
-cd /
-rm -rf ${XSD_BUILD_AREA}
+echo "Install Xerces-C..."
+sudo make install
+
+echo "Xerces-C installed successfully."
